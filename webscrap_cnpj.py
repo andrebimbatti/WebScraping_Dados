8377d703b56b3d08a9556f99ea44c9b3bd464b9a
Andre Bimbatti <andrebimbatti87@gmail.com>
2024-08-21 22:17:49 -0300

    Organizando o código

--- webscrap_cnpj.py
+++ webscrap_cnpj.py
@@ -4,9 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
-import zipfile
 from tqdm import tqdm
 from rich import print
+import zipfile
 import os
 
 
